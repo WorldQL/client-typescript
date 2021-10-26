9f52550e8f27bb33d6e1d5a34dcc53f0a507506c
Jack Baron <jackmbaron@gmail.com>
2021-10-26 09:59:31 +0100

    import flatbuffer code

---
+++ src/worldql-fb/index.ts
@@ -0,0 +1,4 @@
+export { Entity, EntityT } from './messages/entity'
+export { Message, MessageT } from './messages/message'
+export { Record, RecordT } from './messages/record'
+export { Vec3d, Vec3dT } from './messages/vec3d'
