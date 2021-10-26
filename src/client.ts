0e759acbf4ccc0bfb1f9bda78ae11e8d7156989c
Jack Baron <jackmbaron@gmail.com>
2021-10-26 10:41:27 +0100

    add explicit connect/disconnect methods

--- src/client.ts
+++ src/client.ts
@@ -11,8 +11,8 @@ export class Client extends EventEmitter<Events> {
 
   constructor(ws: string) {
     super()
+    this._ws = new ReconnectingWebSocket(ws, [], { startClosed: true })
 
-    this._ws = new ReconnectingWebSocket(ws)
     this._ws.addEventListener('open', () => {
       this.emit('connected')
     })
@@ -20,5 +20,14 @@ export class Client extends EventEmitter<Events> {
     this._ws.addEventListener('error', ({ error }) => {
       this.emit('error', error)
     })
+  }
+
+  public connect(): void {
+    if (this._ws.readyState === ReconnectingWebSocket.OPEN) return
+    this._ws.reconnect()
+  }
+
+  public disconnect(): void {
+    this._ws.close()
   }
 }
