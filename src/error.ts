9172fb5a490bfca07b52bee763d8f7aa375adc42
Jack Baron <jackmbaron@gmail.com>
2022-02-11 13:56:58 +0000

    add function to check if an error is ClientError

--- src/error.ts
+++ src/error.ts
@@ -1,7 +1,29 @@
 import { type Error as ErrorInterface } from './types/clientBound.js'
 
+export const errorSymbol = Symbol('ClientError')
+// @ts-expect-error Type Predicate
+export const isClientError: (
+  error: unknown
+) => error is ClientError = error => {
+  // Check primitive types
+  if (error === undefined) return false
+  if (error === null) return false
+  if (typeof error !== 'object') return false
+
+  // Check it inherits Error
+  if (!(error instanceof Error)) return false
+  if (!(error instanceof ClientError)) return false
+
+  // Check symbol exists
+  return error[errorSymbol]
+}
+
 export class ClientError extends Error {
   public readonly code: number
+
+  public get [errorSymbol]() {
+    return true
+  }
 
   constructor(error: ErrorInterface) {
     super(error.message)
