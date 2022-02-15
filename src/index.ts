70dceb974f8b1cd7cd59a2e4b7a1471ec01a43e0
Jack Baron <jackmbaron@gmail.com>
2022-02-15 18:20:02 +0000

    export more things

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { Client, ClientOptions } from './client.js'
 export { ClientError, isClientError } from './error.js'
+export { Replication, Record, PartialRecord, Vector3 } from './types/common.js'
+export { Uuid, generateUuid, uuidString } from './utils/uuid.js'
